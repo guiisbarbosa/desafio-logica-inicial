1022c458e12f16faaebe38f8f2e0be6bda59b0de
guiisbarbosa <gui.silva93@hotmail.com>
2023-12-06 22:46:56 -0300

    add: app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+let nomeHeroi = "Gui"
+let xpHeroi = 1001
+let nivelHeroi
+
+switch (true) {
+    case xpHeroi < 1000:
+        nivelHeroi = "Ferro"
+        break
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        nivelHeroi = "Bronze"
+        break
+    case xpHeroi >= 2001 && xpHeroi <= 6000:
+        nivelHeroi = "Prata"
+        break
+    case xpHeroi >= 6001 && xpHeroi <= 7000:
+        nivelHeroi = "Ouro"
+        break
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+        nivelHeroi = "Platina"
+        break
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+        nivelHeroi = "Ascendente"
+        break
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+        nivelHeroi = "Imortal"
+        break
+    case xpHeroi > 10000:
+        nivelHeroi = "Radiante"
+}
+
+console.log('O Herói de nome ' + nomeHeroi + ' está no nível de ' + nivelHeroi);
